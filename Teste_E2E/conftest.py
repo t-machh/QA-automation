6c99895d833a2b9d687b1bb26c672b1fdbb8de64
Talyson <talysonmachado.b@gmail.com>
2026-05-05 23:58:49 -0300

    accommodation second attempt

--- Teste_E2E/conftest.py
+++ Teste_E2E/conftest.py
@@ -2,24 +2,33 @@ import os
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 
 @pytest.fixture(scope="function")
 def driver():
-    options = Options()
-    options.add_experimental_option("prefs", {
+    chrome_options = Options()
+    chrome_options.add_experimental_option("prefs", {
         "credentials_enable_service": False,
         "profile.password_manager_enabled": False,
         "profile.password_manager_leak_detection": False
     })
+    
     if os.environ.get("HEADLESS", "false").lower() == "true":
-        options.add_argument("--headless")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--start-maximized")
-
-    service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=options)
+        chrome_options.add_argument("--headless")
+    
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--disable-software-rasterizer")
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--window-size=1920,1080")
+    
+    chrome_binary = os.environ.get("CHROME_BIN", "/usr/local/bin/chrome")
+    chrome_options.binary_location = chrome_binary
+    
+    chromedriver_path = os.environ.get("CHROMEDRIVER_PATH", "/usr/local/bin/chromedriver")
+    service = Service(executable_path=chromedriver_path)
+    
+    driver = webdriver.Chrome(service=service, options=chrome_options)
     yield driver
     driver.quit()
