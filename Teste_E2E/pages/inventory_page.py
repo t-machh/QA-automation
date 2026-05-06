cf53a8f921422b12d9bcda9cca57f08520ff3202
T.Mach <163660399+t-machh@users.noreply.github.com>
2026-05-06 07:46:01 -0300

    Update new inventory_page.py

--- Teste_E2E/pages/inventory_page.py
+++ Teste_E2E/pages/inventory_page.py
@@ -1,41 +1,28 @@
-import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
 class InventoryPage:
+
     def __init__(self, driver):
         self.driver = driver
         self.wait = WebDriverWait(driver, 10)
 
     def is_loaded(self):
+        self.wait.until(EC.url_contains("inventory"))
         return "inventory" in self.driver.current_url
 
     def add_all_products(self):
-        add_buttons = self.driver.find_elements(By.CSS_SELECTOR, "button.btn_inventory")
-        for button in add_buttons:
-            if button.text == "Add to cart":
-                button.click()
-
-    def add_specific_products(self, product_names):
-        for product_name in product_names:
-            xpath = f"//div[@class='inventory_item']//div[text()='{product_name}']/ancestor::div[@class='inventory_item']//button"
-            self.wait.until(EC.element_to_be_clickable((By.XPATH, xpath))).click()
-
-    def get_cart_count(self):
-        cart_badge = self.driver.find_elements(By.CLASS_NAME, "shopping_cart_badge")
-        if cart_badge:
-            return int(cart_badge[0].text)
-        return 0
+        self.wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "btn_inventory")))
+        buttons = self.driver.find_elements(By.CLASS_NAME, "btn_inventory")
+        for btn in buttons:
+            self.wait.until(EC.element_to_be_clickable(btn))
+            btn.click()
 
     def go_to_cart(self):
-        # Aguarda o ícone do carrinho estar clicável
-        cart_icon = self.wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "shopping_cart_link")))
-        # Rola até o elemento para garantir visibilidade
-        self.driver.execute_script("arguments[0].scrollIntoView(true);", cart_icon)
-        time.sleep(0.3)
-        try:
-            cart_icon.click()
-        except:
-            self.driver.execute_script("arguments[0].click();", cart_icon)
-        self.wait.until(EC.url_contains("cart.html"))
+        self.wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "shopping_cart_link"))).click()
+
+    def get_cart_count(self):
+        badge = self.driver.find_elements(By.CLASS_NAME, "shopping_cart_badge")
+        return int(badge[0].text) if badge else 0
