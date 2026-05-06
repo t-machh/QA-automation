36a2d1c7fe67554d02be453f1d193a5855813579
T.Mach <163660399+t-machh@users.noreply.github.com>
2026-05-06 07:43:51 -0300

    new conftest.py

--- Teste_E2E/conftest.py
+++ Teste_E2E/conftest.py
@@ -1,34 +1,25 @@
-import os
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
+
 
 @pytest.fixture(scope="function")
 def driver():
-    chrome_options = Options()
-    chrome_options.add_experimental_option("prefs", {
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--disable-features=PasswordLeakDetection")
+    options.add_experimental_option("prefs", {
         "credentials_enable_service": False,
         "profile.password_manager_enabled": False,
-        "profile.password_manager_leak_detection": False
     })
-    
-    if os.environ.get("HEADLESS", "false").lower() == "true":
-        chrome_options.add_argument("--headless")
-    
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--disable-software-rasterizer")
-    chrome_options.add_argument("--disable-extensions")
-    chrome_options.add_argument("--window-size=1920,1080")
-    
-    chrome_binary = os.environ.get("CHROME_BIN", "/usr/local/bin/chrome")
-    chrome_options.binary_location = chrome_binary
-    
-    chromedriver_path = os.environ.get("CHROMEDRIVER_PATH", "/usr/local/bin/chromedriver")
-    service = Service(executable_path=chromedriver_path)
-    
-    driver = webdriver.Chrome(service=service, options=chrome_options)
+
+    driver = webdriver.Chrome(options=options)
+    driver.maximize_window()
+
     yield driver
+
     driver.quit()
