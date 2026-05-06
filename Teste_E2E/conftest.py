612e29fcf420c18b5de9f8f4c9329e5cd9e3c1e4
Talyson <talysonmachado.b@gmail.com>
2026-05-05 23:55:28 -0300

    accommodation

--- Teste_E2E/conftest.py
+++ Teste_E2E/conftest.py
@@ -1,22 +1,25 @@
+import os
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 
 @pytest.fixture(scope="function")
 def driver():
-    chrome_options = Options()
-    
-    chrome_options.add_experimental_option("prefs", {
+    options = Options()
+    options.add_experimental_option("prefs", {
         "credentials_enable_service": False,
         "profile.password_manager_enabled": False,
         "profile.password_manager_leak_detection": False
     })
-    
-    # chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--start-maximized")
+    if os.environ.get("HEADLESS", "false").lower() == "true":
+        options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--start-maximized")
 
-    driver = webdriver.Chrome(options=chrome_options)
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=options)
     yield driver
     driver.quit()
